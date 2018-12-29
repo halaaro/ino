24ef8d4c562f8ed5374e1f3e8d3f724a43bc27f4
mechdude <15240680+mechdude@users.noreply.github.com>
2018-12-28 19:42:01 -0600

    chore: setup eslint standard

--- index.js
+++ index.js
@@ -3,81 +3,82 @@ const axios = require('axios').default
 const axiosCookieJarSupport = require('@3846masa/axios-cookiejar-support').default
 var xml2js = require('xml-js').xml2js
 
-const tough = require('tough-cookie');
+const tough = require('tough-cookie')
 
-axiosCookieJarSupport(axios);
+axiosCookieJarSupport(axios)
 
-const cookieJar = new tough.CookieJar();
-
+const cookieJar = new tough.CookieJar()
 
 const keys = {
-    attr: 'attr',
-    text: 'value',
-    cdata: 'value'
+  attr: 'attr',
+  text: 'value',
+  cdata: 'value'
 }
 
-const xml2jsOpts = { 
-    compact: true, 
-    attributesKey: keys.attr, 
-    textKey: keys.text,
-    cdataKey: keys.cdata
+const xml2jsOpts = {
+  compact: true,
+  attributesKey: keys.attr,
+  textKey: keys.text,
+  cdataKey: keys.cdata
 }
 
 const soapBody = (verb, body) => `
 <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-    <SOAP-ENV:Body><${ verb }>${ body }</${ verb }></SOAP-ENV:Body>
+    <SOAP-ENV:Body><${verb}>${body}</${verb}></SOAP-ENV:Body>
 </SOAP-ENV:Envelope>
 `
 
 class InoServer {
+  constructor (url, database) {
+    this.url = url
+    this.database = database
+  }
 
-    constructor(url, database) {
-        this.url = url
-        this.database = database
+  auth (user, password) {
+    this.user = user
+    this.password = md5(password)
+    return this.soap(
+      'ApplyAML',
+      `<AML>
+        <Item type="User" action="get" select="id">
+          <login_name>${this.user}</login_name>
+        </Item>
+      </AML>`
+    )
+  }
+
+  applyAML (body) { return this.soap('ApplyAML', body) }
+
+  soap (verb, body) {
+    const options = {
+      url: this.url + '/Server/InnovatorServer.aspx',
+      method: 'post',
+      responseType: 'string',
+      headers: {
+        'SOAPaction': verb,
+        'AUTHUSER': this.user,
+        'AUTHPASSWORD': this.password,
+        'DATABASE': this.database
+      },
+      withCredentials: true,
+      jar: cookieJar,
+      data: soapBody(verb, body)
     }
 
-    auth(user, password) {
-        this.user = user,
-        this.password = md5(password)
-        return this.soap(
-            'ApplyAML',
-            `<AML><Item type="User" action="get" select="id"><login_name>${ this.user }</login_name></Item></AML>`)
-    }
+    return axios(options).then(response => {
+      if (response.status !== 200) {
+        throw `request error (${response.status}): ${response.statusText}`
+      }
 
-    applyAML(body) { return this.soap( 'ApplyAML', body ) }
+      const body = xml2js(response.data, xml2jsOpts)['SOAP-ENV:Envelope']['SOAP-ENV:Body']
+      const fault = body['SOAP-ENV:Fault']
+      if (fault != null) {
+        throw `fault: (${fault.faultcode[keys.text]}) ${fault.faultstring[keys.cdata]}`
+      }
 
-    soap(verb, body) {
-
-        const options = {
-            url: this.url + '/Server/InnovatorServer.aspx',
-            method: 'post',
-            responseType: 'string',
-            headers: { 
-                'SOAPaction': verb,
-                'AUTHUSER': this.user,
-                'AUTHPASSWORD': this.password,
-                'DATABASE': this.database
-            },
-            withCredentials: true,
-            jar: cookieJar,
-            data: soapBody( verb, body )
-        }
-
-        return axios(options).then( response => {
-                
-            if (response.status !== 200) {
-                throw `request error (${ response.status }): ${ response.statusText }`
-            }
-            
-            const body = xml2js(response.data, xml2jsOpts)['SOAP-ENV:Envelope']['SOAP-ENV:Body']
-            const fault = body['SOAP-ENV:Fault']
-            if (fault != null) {
-                throw `fault: (${ fault.faultcode[keys.text] }) ${ fault.faultstring[keys.cdata] }`
-            }
-
-            return body.Result
-        })
-    }
+      return body.Result
+    })
+  }
 }
 
 module.exports = InoServer
